c249a87856a7069f794d3e228cc12a61ce7c529e
Rikson <rikuson@users.noreply.github.com>
2021-12-27 00:57:29 +0900

    Add fetch script

---
+++ fetch.js
@@ -0,0 +1,42 @@
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
+function fetch(url) {
+  console.log(`Fetch: ${url}`);
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+    }).on("error", reject);
+  });
+}
+
+function fetchProducts(products) {
+  const [product] = products;
+  if (!product) return;
+  setTimeout(() => {
+    fetch(`https://endoflife.date/api/${product}.json`).then((res) => {
+      writeJson(`${product}.json`, res);
+      fetchProducts(products.slice(1));
+    });
+  }, 1000);
+}
+
+function writeJson(filename, data) {
+  fs.writeFile(path.join(__dirname, 'data', filename), JSON.stringify(data), (err) => {
+    if (err) throw err;
+  });
+}
+
+fetch('https://endoflife.date/api/all.json').then((products) => {
+  writeJson('all.json', products);
+  fetchProducts(products)
+});
