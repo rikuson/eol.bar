1a26e7e3536e2e0a5e8ce86b8a2892440b426da4
Rikson <rikuson@users.noreply.github.com>
2021-12-27 13:14:00 +0900

    Responsive svg width

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const StrGantt = require('gantt/lib/StrGantt').default;
+const { XMLParser, XMLBuilder } = require('fast-xml-parser');
 const products = require('../data/all.json');
 
 const now = new Date();
@@ -33,7 +34,13 @@ router.get('/', (req, res) => {
     { viewMode: 'month' }
   );
   res.setHeader('Content-Type', 'image/svg+xml');
-  res.send(`<svg xmlns="http://www.w3.org/2000/svg">${gantt.render()}</svg>`);
+  const parser = new XMLParser({ ignoreAttributes: false });
+  const { svg } = parser.parse(gantt.render());
+  const builder = new XMLBuilder({ ignoreAttributes: false });
+  delete svg['@_width'];
+  delete svg['@_height'];
+  svg['@_xmlns'] = 'http://www.w3.org/2000/svg';
+  res.send(builder.build({ svg }));
 });
 
 function isDate(str) {
