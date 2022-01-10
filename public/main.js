78714d62e2ead4deaf1f78bfaf7e7ac91911fcc1
Rikson <rikuson@users.noreply.github.com>
2022-01-10 23:59:49 +0900

    Fix showing example

--- public/main.js
+++ public/main.js
@@ -1,16 +1,9 @@
 window.onload = () => {
   const terminalElm = document.getElementsByClassName('terminal')[0];
-  const exampleReq = fetch('/nodejs');
   setTimeout(() => {
     const innerElm = document.getElementsByClassName('typing')[0];
     innerElm.className = '';
-    exampleReq.then((r) => r.text()).then((ascii) => {
-      ascii = ascii.replaceAll('\n      ', '\n');
-      ascii = ascii.replaceAll('\n', '<br />');
-      ascii = ascii.replaceAll('[37m[44m', '<span class="active">');
-      ascii = ascii.replaceAll('[37m[100m', '<span class="maintenance">');
-      ascii = ascii.replaceAll('[49m[39m', '</span>');
-      terminalElm.innerHTML += '<br />' + ascii + '> ';
-    });
+    const response = document.getElementsByClassName('hidden')[0];
+    response.className = '';
   }, 4500);
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
+const { execSync } = require('child_process');
 const express = require('express');
 const router = express.Router();
 const products = require('../data/all.json');
 const { version } = require('../package.json');
 const AnsiHelp = require('../lib/ansi-help');
+const Product = require('../lib/product')
+const AnsiGantt = require('../lib/ansi-gantt');
+const { uniqDate, cloneDate, firstDay, nextMonth } = require('../lib/util');
 
 router.get('/', (req, res) => {
   const ua = req.get('User-Agent');
@@ -16,7 +20,25 @@ router.get('/', (req, res) => {
     res.send(ansi.render());
     return;
   }
-  res.render('index', { version, products: data });
+  const product = new Product(require('../data/nodejs.json'));
+  const rows = product.search('nodejs');
+  const columns = uniqDate(
+    rows.flatMap(({ release, support, eol }) => [
+      release && firstDay(cloneDate(release)),
+      support && firstDay(cloneDate(support)),
+      eol && firstDay(nextMonth(cloneDate(eol))),
+    ]).filter((d) => d).sort((a, b) => a > b ? 1 : -1)
+  );
+  const gantt = new AnsiGantt(rows, columns);
+  const example = ((str) => {
+    str = str.replaceAll('\n      ', '\n');
+    str = str.replaceAll('\n', '<br />');
+    str = str.replaceAll('[37m[44m', '<span class="active">');
+    str = str.replaceAll('[37m[100m', '<span class="maintenance">');
+    str = str.replaceAll('[49m[39m', '</span>');
+    return '<br />' + str + '>';
+  })(gantt.render());
+  res.render('index', { version, products: data, example });
 });
 
 module.exports = router;
