b77fe287de0ce039b958772f51eae4318cb043d1
Rikson <rikuson@users.noreply.github.com>
2022-01-19 16:54:34 +0900

    Fix crashing by invalid expression

--- routes/ansi.js
+++ routes/ansi.js
@@ -13,27 +13,32 @@ router.get('/', async (req, res) => {
     res.status(404).end();
     return;
   }
-  const targets = await Promise.all(parse(req.params.products).map(async (target) => {
-    const exists = products.includes(target.product);
-    return {
-      ...target,
-      data: exists ? JSON.parse(await db.get(target.product)) : [],
-    };
-  }));
-  const rows = targets.flatMap((target) => {
-    const product = new Product(target.data);
-    return product.search(target.product, target.operator, target.value);
-  });
-  const columns = uniqDate(
-    rows.flatMap(({ release, support, eol }) => [
-      release && firstDay(cloneDate(release)),
-      support && firstDay(cloneDate(support)),
-      eol && firstDay(nextMonth(cloneDate(eol))),
-    ]).filter((d) => d).sort((a, b) => a > b ? 1 : -1)
-  );
+  try {
+    const targets = await Promise.all(parse(req.params.products).map(async (target) => {
+      const exists = products.includes(target.product);
+      return {
+        ...target,
+        data: exists ? JSON.parse(await db.get(target.product)) : [],
+      };
+    }));
+    const rows = targets.flatMap((target) => {
+      const product = new Product(target.data);
+      return product.search(target.product, target.operator, target.value);
+    });
+    const columns = uniqDate(
+      rows.flatMap(({ release, support, eol }) => [
+        release && firstDay(cloneDate(release)),
+        support && firstDay(cloneDate(support)),
+        eol && firstDay(nextMonth(cloneDate(eol))),
+      ]).filter((d) => d).sort((a, b) => a > b ? 1 : -1)
+    );
 
-  const gantt = new AnsiGantt(rows, columns);
-  res.send(gantt.render());
+    const gantt = new AnsiGantt(rows, columns);
+    res.send(gantt.render());
+  } catch (err) {
+    console.error(err);
+    res.status(500).end();
+  }
 });
 
 module.exports = router;
--- routes/svg.js
+++ routes/svg.js
@@ -8,28 +8,33 @@ const { uniqDate, cloneDate, firstDay, nextMonth, time } = require('../lib/util'
 const { parse } = require('../lib/tokenizer');
 
 router.get('/', async (req, res) => {
-  const targets = await Promise.all(parse(req.params.products).map(async (target) => {
-    const exists = products.includes(target.product);
-    return {
-      ...target,
-      data: exists ? JSON.parse(await db.get(target.product)) : [],
-    };
-  }));
-  const rows = targets.flatMap((target) => {
-    const product = new Product(target.data);
-    return product.search(target.product, target.operator, target.value);
-  });
-  const columns = uniqDate(
-    rows.flatMap(({ release, support, eol }) => [
-      release && firstDay(cloneDate(release)),
-      support && firstDay(cloneDate(support)),
-      eol && firstDay(nextMonth(cloneDate(eol))),
-    ]).filter((d) => d).sort((a, b) => a > b ? 1 : -1)
-  );
+  try {
+    const targets = (await Promise.all(parse(req.params.products).map(async (target) => {
+      const exists = products.includes(target.product);
+      return {
+        ...target,
+        data: exists ? JSON.parse(await db.get(target.product)) : [],
+      };
+    }))).catch(console.error);
+    const rows = targets.flatMap((target) => {
+      const product = new Product(target.data);
+      return product.search(target.product, target.operator, target.value);
+    });
+    const columns = uniqDate(
+      rows.flatMap(({ release, support, eol }) => [
+        release && firstDay(cloneDate(release)),
+        support && firstDay(cloneDate(support)),
+        eol && firstDay(nextMonth(cloneDate(eol))),
+      ]).filter((d) => d).sort((a, b) => a > b ? 1 : -1)
+    );
 
-  const svg = new Svg(rows, columns);
-  res.setHeader('Content-Type', 'image/svg+xml');
-  res.send(svg.render(req.query.from, req.query.to));
+    const svg = new Svg(rows, columns);
+    res.setHeader('Content-Type', 'image/svg+xml');
+    res.send(svg.render(req.query.from, req.query.to));
+  } catch (err) {
+    console.error(err);
+    res.status(500).end();
+  }
 });
 
 module.exports = router;
