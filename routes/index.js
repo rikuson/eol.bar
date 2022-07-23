132e1cc93d064e0542b1486fab18f4aab80e344a
Rikson <rikuson@users.noreply.github.com>
2022-07-24 00:38:04 +0900

    Remove unnecessary code

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
-const { execSync } = require('child_process');
 const express = require('express');
 const router = express.Router();
 const db = require('../lib/db');
 const data = require('../data.json');
 const { version } = require('../package.json');
 const AnsiHelp = require('../lib/ansi-help');
-const Product = require('../lib/product')
-const AnsiGantt = require('../lib/ansi-gantt');
-const { uniqDate, cloneDate, firstDay, nextMonth } = require('../lib/util');
 
 router.get('/', async (req, res) => {
   const ua = req.get('User-Agent');
@@ -27,25 +23,7 @@ router.get('/', async (req, res) => {
     res.send(ansi.render());
     return;
   }
-  const product = new Product(JSON.parse(await db.get('nodejs')));
-  const rows = product.search('nodejs');
-  const columns = uniqDate(
-    rows.flatMap(({ releaseDate, support, eol }) => [
-      releaseDate && firstDay(cloneDate(releaseDate)),
-      support && firstDay(cloneDate(support)),
-      eol && firstDay(nextMonth(cloneDate(eol))),
-    ]).filter((d) => d).sort((a, b) => a > b ? 1 : -1)
-  );
-  const gantt = new AnsiGantt(rows, columns);
-  const example = ((str) => {
-    str = str.replaceAll('\n      ', '\n');
-    str = str.replaceAll('\n', '<br />');
-    str = str.replaceAll('\x1b[37m\x1b[44m', '<span class="active">');
-    str = str.replaceAll('\x1b[37m\x1b[100m', '<span class="maintenance">');
-    str = str.replaceAll('\x1b[0m', '</span>');
-    return '<br />' + str + '>';
-  })(gantt.render());
-  res.render('index', { version, products, example });
+  res.render('index', { version, products });
 });
 
 module.exports = router;
